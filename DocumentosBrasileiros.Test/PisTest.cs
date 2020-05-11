903bff5aae2cfc80a1855cf2d15f9ea8d3b18523
Thiago Guttierre do Prado <thiago.prado@xpi.com.br>
2020-05-11 16:30:11 -0300

    feat: add tests

---
+++ DocumentosBrasileiros.Test/PisTest.cs
@@ -0,0 +1,24 @@
+using DocumentosBrasileiros.Documentos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DocumentosBrasileiros.Test
+{
+    [TestClass]
+    public class PisTest
+    {
+        [TestMethod]
+        public void PisIsValid()
+        {
+           var doc = new Pis("657.59730.12-6");
+            Assert.IsTrue(doc.DocumentoValido());
+        }
+
+        [TestMethod]
+        public void FakePisIsValid()
+        {
+            var doc = new Pis();
+            doc.GerarFake();
+            Assert.IsTrue(doc.DocumentoValido());
+        }
+    }
+}
